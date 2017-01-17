971cf6bcae26340ed0863f1df7a99911464560f0
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-01-17 19:07:09 +0100

    ProcessLifecycle

---
+++ lib/datamill/process_lifecycle.rb
@@ -0,0 +1,24 @@
+module Datamill
+
+module ProcessLifecycle
+  def self.new(*attributes)
+    result = Class.new(Base) do
+      attr_accessor(*attributes)
+    end.new
+    yield result if block_given?
+    result
+  end
+
+  class Base
+    def setup
+      @setup ||= Proc.new
+    end
+
+    def teardown
+      @teardown ||= Proc.new
+    end
+  end
+end
+
+end
+
