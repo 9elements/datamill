3435c73c277445b01a41336a6ade21cc0688faa7
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-01-17 11:57:13 +0100

    change cell behaviour API: timeout need is signalled via state; there's `.nop` to signal initial timeout need

--- lib/datamill/cell/behaviour.rb
+++ lib/datamill/cell/behaviour.rb
@@ -3,13 +3,13 @@ module Datamill::Cell
 module Behaviour
   # This defines the expected interface for a cell behaviour.
 
+  def self.nop(state)
+  end
+
   def self.handle_message(state, message)
   end
 
   def self.handle_timeout(state)
-  end
-
-  def self.next_timeout(state)
   end
 
   def self.behaviour_name
