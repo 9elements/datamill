674499246591c35ef03ce54d0bafe52f4445b49e
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-01-17 14:54:26 +0100

    clarify what is a "living" cell behaviour function

--- lib/datamill/cell/behaviour.rb
+++ lib/datamill/cell/behaviour.rb
@@ -2,6 +2,8 @@ module Datamill::Cell
 
 module Behaviour
   # This defines the expected interface for a cell behaviour.
+  # All methods which take a state argument are callbacks
+  # needed to implement behaviour of a living cell.
 
   def self.nop(state)
   end
